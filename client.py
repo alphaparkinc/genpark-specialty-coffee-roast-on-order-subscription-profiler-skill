68cdf933f5ad393c7a0e980a6b45db3301aededb
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-08-24 09:33:12 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,12 @@
+class SpecialtyCoffeeRoastOnOrderSubscriptionProfilerClient:
+    def profile_and_roast_batch(self, brew_method='Aeropress', flavor_profile='fruity_floral', subscription_freq_weeks=2):
+        return {
+            'batch_roast_id': 'bt_rst_5519',
+            'estate_origin': 'Attikan Estate, Biligiriranga Hills',
+            'varietal': 'Arabica SLN 9',
+            'roast_level': 'LIGHT_MEDIUM_ROAST',
+            'tasting_notes': ['Wild Berry', 'Jasmine Blossom', 'Honeyed Lemon'],
+            'sca_cupping_score': 87.5,
+            'roast_to_door_freshness_hours': 36.0,
+            'custom_grind_size': 'Medium-Fine for Aeropress'
+        }
